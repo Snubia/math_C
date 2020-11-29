7bed3c8047bb0eb4bcdb2f337e3b376360b72586
Sandrine <nunubabila@gmail.com>
2020-11-29 14:13:58 -0800

    enum

---
+++ 10_enum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+enum day
+{
+    mon,
+    tue,
+    wed,
+    thu,
+    fri,
+    sat,
+    sun
+};
+
+int main()
+{
+    day d;
+    d = mon;
+    cout << d << endl;
+}
